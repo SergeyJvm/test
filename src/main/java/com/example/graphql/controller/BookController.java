0fa04ec218ff823881f54b3aa008489f278230e5
Сергей Пынзарь <SPynzar_tk@inno.local>
2024-10-26 22:19:14 +0300

    Initial commit

--- src/main/java/com/example/graphql/controller/BookController.java
+++ src/main/java/com/example/graphql/controller/BookController.java
@@ -4,26 +4,31 @@ import com.example.graphql.dto.Author;
 import com.example.graphql.dto.Book;
 import java.util.List;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@Slf4j
 public class BookController {
 
     @QueryMapping
     public List<Book> books() {
+        log.info("BookController: get all books");
         return Book.books;
     }
 
     @QueryMapping
     public Optional<Book> bookById(@Argument Integer id) {
+        log.info("BookController: get book by id {}", id);
         return Book.getBookById(id);
     }
 
     @SchemaMapping
     public Optional<Author> author(Book book){
+        log.info("BookController: get author {}", book);
         return Author.getAuthorById(book.authorId());
     }
 }
